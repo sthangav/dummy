77a72f41a244ccfb5bee8789759d5af4c9ef800a
sthangav <t.sathyanarayanan@hotmail.com>
2016-02-26 07:41:25 +0530

    Added Layer color for Akash Watch Face

--- config/js/main.js
+++ config/js/main.js
@@ -17,13 +17,20 @@ function submitHandler() {
 function loadOptions() {
   var $backgroundColorPicker = $('#backgroundColorPicker');
   var $backgroundColorPicker1 = $('#backgroundColorPicker1');
+  var $backgroundColorPicker2 = $('#backgroundColorPicker2');
+  var $backgroundColorPicker3 = $('#backgroundColorPicker3');
+
+
   var $timeFormatCheckbox = $('#timeFormatCheckbox');
 
   if (localStorage.backgroundColor) {
     $backgroundColorPicker[0].value = localStorage.backgroundColor;
        $backgroundColorPicker1[0].value = localStorage.backgroundColor1; 
+       $backgroundColorPicker2[0].value = localStorage.backgroundColor2; 
+       $backgroundColorPicker3[0].value = localStorage.backgroundColor3; 
     $timeFormatCheckbox[0].checked = localStorage.twentyFourHourFormat === 'true';
   }
+
 }
 
 function getAndStoreConfigData() {
@@ -34,11 +41,17 @@ function getAndStoreConfigData() {
   var options = {
     backgroundColor: $backgroundColorPicker.val(),
     backgroundColor1: $backgroundColorPicker1.val(),
+    backgroundColor2: $backgroundColorPicker2.val(),
+    backgroundColor3: $backgroundColorPicker3.val(),
+
     twentyFourHourFormat: $timeFormatCheckbox[0].checked
   };
 
   localStorage.backgroundColor = options.backgroundColor;
   localStorage.backgroundColor1 = options.backgroundColor1;
+  localStorage.backgroundColor2 = options.backgroundColor2;
+  localStorage.backgroundColor3 = options.backgroundColor3;
+
   localStorage.twentyFourHourFormat = options.twentyFourHourFormat;
 
   console.log('Got options: ' + JSON.stringify(options));
